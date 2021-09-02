a292a591412f963cb092e3a03f9b98a215f90351
Zach Antosko <zachary.antosko@gmail.com>
2021-09-02 13:17:26 -0500

    added new algorithm

---
+++ Algorithms/Sorting/Quick-Sort.js
@@ -0,0 +1,34 @@
+// QUICK SORT
+
+/** Big O Complexity
+ * Time - O(n log n) (Best and Average case)
+ * Time - O(n^2) (Worst case)
+ * Space - O(log n) (All cases)
+ */
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+	function swap(array, i, j) {
+		let temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	let pivot = arr[start];
+	let swapIdx = start;
+	for (let i = start + 1; i < arr.length; i++) {
+		if (pivot > arr[i]) {
+			swapIdx++;
+			swap(arr, swapIdx, i);
+		}
+	}
+	swap(arr, start, swapIdx);
+	return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+	if (left < right) {
+		let pivotIdx = pivot(arr, left, right);
+		quickSort(arr, left, pivotIdx - 1);
+		quickSort(arr, pivotIdx + 1, right);
+	}
+	return arr;
+}
